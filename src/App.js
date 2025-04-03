271f7497a6413cf27dd764c88b8ce559e4f075b7
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-04-03 17:47:00 +0800

    添加App主組件

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BallSortGame from './components/BallSortGame';
+
+/**
+ * Main Application Component
+ * Renders the Ball Sort Game
+ */
+function App() {
+  return (
+    <div className="App">
+      <BallSortGame />
+    </div>
+  );
+}
+
+export default App;
